115ba8a9ee6ba0da44319fef2bc3578818040739
sambhavjain108 <152012154+sambhavjain108@users.noreply.github.com>
2025-08-05 03:52:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Sambhav's NFT site now its time to show the world webhook is woerking!"
+    return "Hello from Sambhav's NFT site now its time to show the world webhook is woerki===ng!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
